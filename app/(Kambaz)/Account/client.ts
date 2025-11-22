f3183be6474b1b489d5ffca663466e2c84260aa4
dhruthi.06 <dhruthi.06@gmail.com>
2025-11-22 15:10:17 -0500

    Courses chnages

--- app/(Kambaz)/Account/client.ts
+++ app/(Kambaz)/Account/client.ts
@@ -4,32 +4,30 @@ export const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 export const USERS_API = `${HTTP_SERVER}/api/users`;
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_HTTP_SERVER,
-  withCredentials: true
+  withCredentials: true,
+  baseURL: HTTP_SERVER
 });
 
+// ---- AUTH ----
 
 export const signin = async (credentials: any) => {
   const response = await api.post(`/api/users/signin`, credentials);
   return response.data;
-}
-
-
+};
 
 export const signup = async (user: any) => {
   const response = await api.post(`/api/users/signup`, user);
   return response.data;
 };
 
-
 export const profile = async () => {
   const response = await api.post(`/api/users/profile`);
   return response.data;
 };
 
-
 export const signout = async () => {
-  await api.post(`/api/users/signout`);
+  const response = await api.post(`/api/users/signout`);
+  return response.data;
 };
 
 export const updateUser = async (user: any) => {
