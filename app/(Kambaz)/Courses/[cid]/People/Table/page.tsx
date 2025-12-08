ae5583225e7e6512400ce635dc67ff05dc8f2994
dhruthi.06 <dhruthi.06@gmail.com>
2025-12-07 20:53:04 -0500

    Changes

--- app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
+++ app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -2,29 +2,30 @@
 
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
-import { useParams } from "next/navigation";
-import * as db from "../../../../Database"; // assumes db exports users.json and enrollments.json
+import { useState } from "react";
+import PeopleDetails from "./Details";
 
-interface User {
-  _id: string;
-  firstName: string;
-  lastName: string;
-  loginId: string;
-  section: string;
-  role: string;
-  lastActivity: string;
-  totalActivity: string;
+interface PeopleTableProps {
+  users: any[];
+  fetchUsers: () => void;
 }
 
-export default function PeopleTable() {
-  const { cid } = useParams();
-  const { users }: { users: User[] } = db;
-
-  // Show all users for every course
-  const allUsers = users;
+export default function PeopleTable({ users, fetchUsers }: PeopleTableProps) {
+  const [showDetails, setShowDetails] = useState(false);
+  const [showUserId, setShowUserId] = useState<string | null>(null);
 
   return (
     <div id="wd-people-table" className="p-3">
+      {showDetails && (
+        <PeopleDetails
+          uid={showUserId}
+          onClose={() => {
+            setShowDetails(false);
+            fetchUsers();
+          }}
+        />
+      )}
+
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -36,19 +37,26 @@ export default function PeopleTable() {
             <th>Total Activity</th>
           </tr>
         </thead>
+
         <tbody>
-          {allUsers.map((user) => (
+          {users?.map((user: any) => (
             <tr key={user._id}>
-              <td className="wd-full-name text-nowrap">
+              <td
+                className="wd-full-name text-nowrap"
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  setShowUserId(user._id);
+                  setShowDetails(true);
+                }}
+              >
                 <FaUserCircle className="me-2 fs-1 text-secondary" />
-                <span className="wd-first-name">{user.firstName}</span>{" "}
-                <span className="wd-last-name">{user.lastName}</span>
+                {user.firstName} {user.lastName}
               </td>
-              <td className="wd-login-id">{user.loginId}</td>
-              <td className="wd-section">{user.section}</td>
-              <td className="wd-role">{user.role}</td>
-              <td className="wd-last-activity">{user.lastActivity}</td>
-              <td className="wd-total-activity">{user.totalActivity}</td>
+              <td>{user.loginId}</td>
+              <td>{user.section}</td>
+              <td>{user.role}</td>
+              <td>{user.lastActivity}</td>
+              <td>{user.totalActivity}</td>
             </tr>
           ))}
         </tbody>
